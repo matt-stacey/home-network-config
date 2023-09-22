f19fe5bdee345e4152f5faf61da6353f01e94a7d
TUF-win <53570754+matt-stacey@users.noreply.github.com>
2023-09-22 12:17:29 -0500

    better logging

--- containerization/logger.py
+++ containerization/logger.py
@@ -1,5 +1,9 @@
 import logging
 
+from pathlib import Path
+
+from data import Paths
+
 
 class Logger:
     def __init__(self, name: str):
@@ -8,9 +12,25 @@ class Logger:
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.INFO)
         self.logger.propagate = False
+
         self.formatter = logging.Formatter('%(asctime)s: %(name)s: %(levelname)s: %(message)s')  # type: ignore
+
+        self.log_file: Path = Paths.logs_dir / f'{self.logger_name}.log'
+        self.log_file.parent.mkdir(parents=True, exist_ok=True)
+
+        self.file_handler = logging.FileHandler(self.log_file, mode='w')  # type: ignore
+        self.file_handler.setLevel(logging.DEBUG)
+        self.file_handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.file_handler)
+
         self.stream_handler = logging.StreamHandler()  # type: ignore
+        self.stream_handler.setLevel(logging.INFO)
         self.stream_handler.setFormatter(self.formatter)
         self.logger.addHandler(self.stream_handler)
 
-        # TODO log to file
+    def log_and_raise(self, error_message: str):
+        self.logger.error(error_message)
+        raise RuntimeError(error_message)
+
+    def warn_and_continue(self, warning_message: str):
+        self.logger.warning(warning_message)
