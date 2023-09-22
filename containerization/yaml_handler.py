ca61974c42a75fd9e73ef607eb130bc1bd031057
TUF-win <53570754+matt-stacey@users.noreply.github.com>
2023-09-22 11:24:55 -0500

    yaml handler JIT

--- containerization/yaml_handler.py
+++ containerization/yaml_handler.py
@@ -1,14 +1,19 @@
 import yaml
 
+from pathlib import Path
+
 
 class YamlFile:
     def __init__(self, yaml_path: Path):
         self.yaml_path: Path = yaml_path
+        self._data = None  # type: ignore
 
+    def _load_data(self):
         with self.yaml_path.open('r') as f:
             self._data = yaml.safe_load(f.read())
 
     @property
     def data(self):
-        # TODO jit
+        if self._data is None:
+            self._load_data()
         return self._data
