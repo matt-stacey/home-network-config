9f1bd486d84a8fe50938d1daa8036d9539ee4824
TUF-win <53570754+matt-stacey@users.noreply.github.com>
2023-09-22 12:02:57 -0500

    remove creator, create master

--- containerization/container_setup.py
+++ containerization/container_setup.py
@@ -1,14 +1,19 @@
+import os
 import logging
 import argparse
+
+from time import perf_counter
+from typing import List
 from pathlib import Path
 
 from data import Paths, Defaults
+from logger import Logger
 from yaml_handler import YamlFile
-from creator import ContainerCreator
 from configurer import ContainerConfigurer
 # TODO combine into ContainerMaster, place here; pull in yaml handling/logging
 
 
+# TODO remove logging here, use master
 logger = logging.getLogger('main')
 logger.setLevel(logging.INFO)
 FORMAT = '%(asctime)s: %(name)s: %(levelname)s: %(message)s'
@@ -25,29 +30,69 @@ def make_parser():
     return parser
 
 
-def main():
-    parser = make_parser()
-    args = parser.parse_args()
+class ContainerMaster(Logger):
+    def __init__(self, yaml_path: Path, sls_template_dir: str):
+        super().__init__('container_master')
+        self.container_roster: YamlFile = YamlFile(yaml_path)
+        self.desired_containers = self.container_roster.data
 
-    container_roster = YamlFile(Path(args.yaml))
-    creator = ContainerCreator(container_roster)
-    configurer = ContainerConfigurer(container_roster, Paths.sls_template_dir)
+    def copy_from(self, source_container: str) -> List[str]:
+        if source_container not in self.current_containers:
+            raise RuntimeError(f'Source container {source_container} does not exist!')
 
-    logger.info(creator.current_containers)
-    logger.info(creator.desired_containers.keys())
+        containers_before_copy: List[str] = list(self.current_containers)
+        created_containers: List[str] = []
+        copy_cmd: str = 'lxc-copy -n {source_container} -N {new_container}'
 
-    # TODO add arg to put in source container
-    # TODO create MESG to copy in args
-    created = creator.copy_from(Defaults.source_container)
-    logger.info(f'Created containers: {created}')
+        for new_container in self.desired_containers.keys():
+            if new_container in containers_before_copy:
+                self.logger.warning(f'{new_container} already exists; skipping!')
+                continue
 
-    logger.info(configurer.sls_templates.keys())
-    # TODO configure MESG in args
+            t_start: float = perf_counter()
+            self.logger.info(f'Copying {source_container} to {new_container}')
+            result = os.popen(copy_cmd.format(source_container=source_container,
+                                              new_container=new_container))
+            if len(result.read()) == 0:  # FIXME very weak, but dunno what failing looks like yet
+                created_containers.append(new_container)
+            t_stop: float = perf_counter()
+            self.logger.info(f'Copied {new_container} in {t_stop-t_start} seconds')
 
-    # TODO turn them on (parser arg?)
-    
-    # TODO? output commands to attach
+        return sorted(created_containers)
+
+    @property
+    def current_containers(self) -> List[str]:
+        lxc_cmd: str = 'lxc-ls -f'
+        lxc_output = os.popen(lxc_cmd)  # type: ignore
+        lxc_data: List[str] = lxc_output.read().split('\n')[1:]
+        containers: List[str] = [cont_info.split(' ')[0] for cont_info in lxc_data if len(cont_info) > 0]
+        return containers
 
 
 if __name__ == '__main__':
-    main()  # TODO instance to run
+    parser = make_parser()
+    args = parser.parse_args()
+
+    containter_master = ContainerMaster(Path(args.yaml), Paths.sls_template_dir)
+
+    containter_master.logger.info(containter_master.current_containers)
+    containter_master.logger.info(containter_master.desired_containers.keys())
+
+    container_roster = YamlFile(Path(args.yaml))
+    configurer = ContainerConfigurer(container_roster, Paths.sls_template_dir)
+
+    # TODO add arg to put in source container
+    # TODO create MESG to copy in args
+    created = []
+    try:
+        created = containter_master.copy_from(Defaults.source_container)
+    except RuntimeError as err:
+        containter_master.logger.warning(err)
+    containter_master.logger.info(f'Created containers: {created}')
+
+    containter_master.logger.info(configurer.sls_templates.keys())
+    # TODO configure MESG in args
+
+    # TODO turn them on (parser arg?)
+
+    # TODO? output commands to attach
