eacbfe9cb9d9236ab1d9b442790b82d53e49d66b
TUF-win <53570754+matt-stacey@users.noreply.github.com>
2024-01-04 07:02:25 -0600

    lxc paths

--- containerization/data.py
+++ containerization/data.py
@@ -8,7 +8,13 @@ class Paths:
     sls_template_dir: str = 'templates/'  # trailing slash for Jinja2
     sls_template_suffix: str = '.template'
 
+    lxc_data: Path = Path('/var/lib/lxc/')
+
     logs_dir: Path = containerization / 'logs'
+
+    @staticmethod
+    def container_data(container):
+        return Paths.lxc_data / container
 
 class Defaults:
     source_container: str = 'x553-minion-bootstrapper'
