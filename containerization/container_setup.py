84e0ca96c96f1222956d334b39602e487c6d4bb4
TUF-win <53570754+matt-stacey@users.noreply.github.com>
2024-02-14 03:58:40 -0600

    clean up setup script

--- containerization/container_setup.py
+++ containerization/container_setup.py
@@ -1,9 +1,12 @@
+import logging
 import argparse
 
 from pathlib import Path
 
 from container_master import ContainerMaster
+
 from data import Defaults
+from logger import Logger
 
 
 def make_parser():
@@ -34,41 +37,42 @@ if __name__ == '__main__':
 
     container_master = ContainerMaster(Path(args.yaml))
 
-    container_master.logger.info(container_master.current_containers)
+    container_setup = Logger('container_setup')
+    logger = container_setup.logger  # janky
+    logger.setLevel(logging.DEBUG)
+    logger.info(container_master.current_containers)
 
     if args.copy:
-        container_master.logger.info(f'Copying containers from {args.source_container}')
-        container_master.logger.info(container_master.desired_containers.keys())
-
-        created = []  # FIXME remove
-        try:  # FIXME remove
-            created = container_master.copy_from(args.source_container)
-        except RuntimeError as err:  # FIXME remove
-            container_master.logger.warning(err)  # FIXME remove
-        container_master.logger.info(f'Created containers: {created}')
+        logger.info(f'Copying containers from {args.source_container}')
+        logger.debug(container_master.desired_containers.keys())
+        created = container_master.copy_from(args.source_container)
+        logger.info(f'Created containers: {created}')
 
     elif args.configure:
-        container_master.logger.info('Configuring containers')
-        container_master.logger.info(container_master.sls_templates.keys())
-        container_master.configure()
+        logger.info('Configuring containers')
+        logger.debug(container_master.sls_templates.keys())
+        configured = container_master.configure()
+        logger.info(f'Configured containers: {configured}')
 
     elif args.activate:
-        container_master.logger.info('Activating containers')
+        logger.info('Activating containers')
         activated = container_master.activate_containers(True)
-        container_master.logger.info(f'Activated containers: {activated}')
-        container_master.logger.info('To attach to a container, run:')
+        logger.info(f'Activated containers: {activated}')
+
+        # Show the user how to get into each container
+        logger.info('To attach to a container, run:')
         for container in activated:
-            container_master.logger.info(f'lxc-attach -n {container}')
+            logger.info(f'lxc-attach -n {container}')
 
     elif args.deactivate:
-        container_master.logger.info('Dectivating containers')
+        logger.info('Dectivating containers')
         deactivated = container_master.activate_containers(False)
-        container_master.logger.info(f'Deactivated containers: {deactivated}')
+        logger.info(f'Deactivated containers: {deactivated}')
 
     elif args.start_salt:
-        container_master.logger.info('Starting Salt on all containers')
+        logger.info('Starting Salt on all containers')
         salted = container_master.start_salt_containers(False)
-        container_master.logger.info(f'Salt started on containers: {salted}')
+        logger.info(f'Salt started on containers: {salted}')
 
     else:
-        container_master.logger.info('No action given!')
+        logger.info('No action given!')
